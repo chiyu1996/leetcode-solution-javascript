113596e3149aa2acdfb2a8856ec9989fdde3c3cf
chiyu1996 <172302374@qq.com>
2020-10-12 16:50:51 +0800

    2020.10.12 二叉树搜索树的最小绝对差&&最大子矩阵

---
+++ editor/cn/[783]二叉搜索树节点最小距离.js
@@ -0,0 +1,76 @@
+//给定一个二叉搜索树的根节点 root，返回树中任意两节点的差的最小值。 
+//
+// 
+//
+// 示例： 
+//
+// 输入: root = [4,2,6,1,3,null,null]
+//输出: 1
+//解释:
+//注意，root是树节点对象(TreeNode object)，而不是数组。
+//
+//给定的树 [4,2,6,1,3,null,null] 可表示为下图:
+//
+//          4
+//        /   \
+//      2      6
+//     / \    
+//    1   3  
+//
+//最小的差值是 1, 它是节点1和节点2的差值, 也是节点3和节点2的差值。 
+//
+// 
+//
+// 注意： 
+//
+// 
+// 二叉树的大小范围在 2 到 100。 
+// 二叉树总是有效的，每个节点的值都是整数，且不重复。 
+// 本题与 530：https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/ 
+//相同 
+// 
+// Related Topics 树 递归 
+// 👍 88 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDiffInBST = function (root) {
+    let pre;
+    let ans = Infinity;
+    let preVal = Infinity;
+    while (root) {
+        if (root.left) {
+            pre = root.left;
+            while (pre.right && pre.right !== root) {
+                pre = pre.right;
+            }
+            if (!pre.right) {
+                pre.right = root;
+                root = root.left;
+
+            } else {
+                ans = Math.min(ans, Math.abs(root.val - preVal));
+                preVal = root.val;
+                pre.right = null;
+                root = root.right;
+            }
+        } else {
+            ans = Math.min(ans, Math.abs(root.val - preVal));
+            preVal = root.val;
+            root = root.right;
+        }
+    }
+    return ans;
+};
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ editor/cn/[面试题 17.24]最大子矩阵.js
@@ -0,0 +1,63 @@
+//给定一个正整数、负整数和 0 组成的 N × M 矩阵，编写代码找出元素总和最大的子矩阵。 
+//
+// 返回一个数组 [r1, c1, r2, c2]，其中 r1, c1 分别代表子矩阵左上角的行号和列号，r2, c2 分别代表右下角的行号和列号。若有多个满
+//足条件的子矩阵，返回任意一个均可。 
+//
+// 注意：本题相对书上原题稍作改动 
+//
+// 示例： 
+//
+// 输入：
+//[
+//   [-1,0],
+//   [0,-1]
+//]
+//输出：[0,1,0,1]
+//解释：输入中标粗的元素即为输出所表示的矩阵 
+//
+// 
+//
+// 说明： 
+//
+// 
+// 1 <= matrix.length, matrix[0].length <= 200 
+// 
+// Related Topics 动态规划 
+// 👍 31 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var getMaxMatrix = function (matrix) {
+    let n = matrix.length;
+    let m = matrix[0].length;
+    let d = new Array(m).fill(0);
+    let maxSum = -Infinity;
+    let r1, c1;
+    let ans, sum;
+    for (let i = 0; i < n; i++) {
+        for (let t = 0; t < m; t++) d[t] = 0;
+        for (let j = i; j < n; j++) {
+            sum = 0;
+            for (let k = 0; k < m; k++) {
+                d[k] += matrix[j][k];
+                if (sum > 0) {
+                    sum += d[k];
+                } else {
+                    sum = d[k];
+                    r1 = i;
+                    c1 = k;
+                }
+                if (sum > maxSum) {
+                    maxSum = sum;
+                    ans = [r1, c1, j, k];
+                }
+            }
+        }
+    }
+    return ans;
+};
+//leetcode submit region end(Prohibit modification and deletion)
