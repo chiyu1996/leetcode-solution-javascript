6d6dc7752617e9d705b57db1cfca500feeee7d39
chiyu1996 <172302374@qq.com>
2020-10-21 13:37:49 +0800

    2020.10.21 因病缺勤几天打卡,143&&844&&925

--- editor/cn/[844]比较含退格的字符串.js
+++ editor/cn/[844]比较含退格的字符串.js
@@ -60,8 +60,8 @@
  * @param {string} T
  * @return {boolean}
  */
+//栈
 var backspaceCompare = function (S, T) {
-
     const getResult = (s) => {
         const stack = [];
         for (let i = 0; i < s.length; i++) {
@@ -76,5 +76,46 @@ var backspaceCompare = function (S, T) {
         return stack.join("");
     };
     return getResult(S) === getResult(T);
+};
+//双指针
+var backspaceCompare = function (S, T) {
+    let i = S.length - 1, j = T.length - 1;
+    let skipS = 0, skipT = 0;
+    while (i >= 0 || j >= 0) {
+        while (i >= 0) {
+            if (S[i] === "#") {
+                skipS++;
+                i--;
+            } else if (skipS > 0) {
+                skipS--;
+                i--;
+            } else {
+                break;
+            }
+        }
+        while (j >= 0) {
+            if (T[j] === "#") {
+                skipT++;
+                j--;
+            } else if (skipT > 0) {
+                skipT--;
+                j--;
+            } else {
+                break;
+            }
+        }
+        if (i >= 0 && j >= 0) {
+            if (S[i] !== T[j]) {
+                return false;
+            }
+        } else {
+            if (i >= 0 || j >= 0) {
+                return false;
+            }
+        }
+        i--;
+        j--;
+    }
+    return true;
 };
 //leetcode submit region end(Prohibit modification and deletion)
