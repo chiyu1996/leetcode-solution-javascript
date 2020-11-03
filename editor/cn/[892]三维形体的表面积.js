a7c51e76b3c78ac21804763a8642cdcfd60b6a59
chiyu1996 <172302374@qq.com>
2020-11-03 14:47:35 +0800

    2020.11.3 892(数组) 941(数学) 1447(数学)

---
+++ editor/cn/[892]三维形体的表面积.js
@@ -0,0 +1,79 @@
+//在 N * N 的网格上，我们放置一些 1 * 1 * 1 的立方体。 
+//
+// 每个值 v = grid[i][j] 表示 v 个正方体叠放在对应单元格 (i, j) 上。 
+//
+// 请你返回最终形体的表面积。 
+//
+// 
+//
+// 
+// 
+//
+// 示例 1： 
+//
+// 输入：[[2]]
+//输出：10
+// 
+//
+// 示例 2： 
+//
+// 输入：[[1,2],[3,4]]
+//输出：34
+// 
+//
+// 示例 3： 
+//
+// 输入：[[1,0],[0,2]]
+//输出：16
+// 
+//
+// 示例 4： 
+//
+// 输入：[[1,1,1],[1,0,1],[1,1,1]]
+//输出：32
+// 
+//
+// 示例 5： 
+//
+// 输入：[[2,2,2],[2,1,2],[2,2,2]]
+//输出：46
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= N <= 50 
+// 0 <= grid[i][j] <= 50 
+// 
+// Related Topics 几何 数学 
+// 👍 130 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var surfaceArea = function (grid) {
+    if (!grid || !grid.length) return 0;
+    let n = grid.length;
+    let ans = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] > 0) {
+                ans += grid[i][j] * 4 + 2;
+                if (i > 0) {
+                    ans -= Math.min(grid[i][j], grid[i - 1][j]) * 2;
+                }
+                if (j > 0) {
+                    ans -= Math.min(grid[i][j], grid[i][j - 1]) * 2;
+                }
+            }
+
+        }
+    }
+    return ans;
+};
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ editor/cn/[941]有效的山脉数组.js
@@ -0,0 +1,74 @@
+//给定一个整数数组 A，如果它是有效的山脉数组就返回 true，否则返回 false。 
+//
+// 让我们回顾一下，如果 A 满足下述条件，那么它是一个山脉数组： 
+//
+// 
+// A.length >= 3 
+// 在 0 < i < A.length - 1 条件下，存在 i 使得：
+// 
+// A[0] < A[1] < ... A[i-1] < A[i] 
+// A[i] > A[i+1] > ... > A[A.length - 1] 
+// 
+// 
+// 
+//
+// 
+//
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：[2,1]
+//输出：false
+// 
+//
+// 示例 2： 
+//
+// 输入：[3,5,5]
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 输入：[0,3,2,1]
+//输出：true 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= A.length <= 10000 
+// 0 <= A[i] <= 10000 
+// 
+//
+// 
+//
+// 
+// Related Topics 数组 
+// 👍 93 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} A
+ * @return {boolean}
+ */
+var validMountainArray = function (A) {
+    let n = A.length;
+    let i = 0;
+    while (i < n - 1 && A[i] < A[i + 1]) {
+        i++;
+    }
+    if (i === 0 || i === n - 1) {
+        return false;
+    }
+    while (i < n - 1 && A[i] > A[i + 1]) {
+        i++;
+    }
+    return i === n - 1;
+
+};
+//leetcode submit region end(Prohibit modification and deletion)
