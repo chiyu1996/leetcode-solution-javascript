1de7576b020cbd89ed652fc378f7aa7ed7e9c7e4
chiyu1996 <172302374@qq.com>
2020-11-15 13:39:32 +0800

    2020.11.15 402(单调栈)

---
+++ editor/cn/[402]移掉K位数字.js
@@ -0,0 +1,66 @@
+// 给定一个以字符串表示的非负整数 num，移除这个数中的 k 位数字，使得剩下的数字最小。
+//
+// 注意:
+//
+//
+// num 的长度小于 10002 且 ≥ k。
+// num 不会包含任何前导零。
+//
+//
+// 示例 1 :
+//
+//
+// 输入: num = "1432219", k = 3
+// 输出: "1219"
+// 解释: 移除掉三个数字 4, 3, 和 2 形成一个新的最小的数字 1219。
+//
+//
+// 示例 2 :
+//
+//
+// 输入: num = "10200", k = 1
+// 输出: "200"
+// 解释: 移掉首位的 1 剩下的数字为 200. 注意输出不能有任何前导零。
+//
+//
+// 示例 3 :
+//
+//
+// 输入: num = "10", k = 2
+// 输出: "0"
+// 解释: 从原数字移除所有的数字，剩余为空就是0。
+//
+// Related Topics 栈 贪心算法
+// 👍 389 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+var removeKdigits = function(num, k) {
+  const stack = [];
+  for (const digit of num) {
+    while (stack.length > 0 && stack[stack.length - 1] > digit && k) {
+      stack.pop();
+      k--;
+    }
+    stack.push(digit);
+  }
+  while (k > 0) {
+    stack.pop();
+    k--;
+  }
+  let ans = '';
+  let frontZero = true;
+  for (const digit of stack) {
+    if (frontZero && digit === '0') {
+      continue;
+    }
+    frontZero = false;
+    ans += digit;
+  }
+  return ans === '' ? '0' : ans;
+};
+// leetcode submit region end(Prohibit modification and deletion)
