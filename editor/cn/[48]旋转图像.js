878ea20177c15810752e02bf57194ca297e04cff
chiyu1996 <172302374@qq.com>
2020-12-19 14:52:53 +0800

    2020.12.17 1107(字符串api) 2020.12.18 389(数学) 733(dfs) 2020.12.19 48(矩阵)

--- editor/cn/[48]旋转图像.js
+++ editor/cn/[48]旋转图像.js
@@ -50,19 +50,15 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-  const n = matrix.length;
+  const n = matrix.length
   for (let i = 0; i < n; i++) {
     for (let j = i; j < n; j++) {
-      const tmp = matrix[i][j];
-      matrix[i][j] = matrix[j][i];
-      matrix[j][i] = tmp;
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
     }
   }
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n / 2; j++) {
-      const tmp = matrix[i][j];
-      matrix[i][j] = matrix[i][n - 1 - j];
-      matrix[i][n - 1 - j] = tmp;
+      [matrix[i][j], matrix[i][n - 1 - j]] = [matrix[i][n - 1 - j], matrix[i][j]];
     }
   }
   return matrix;
