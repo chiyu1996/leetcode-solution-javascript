7edfdf98f22dd70c57980316295088ae208845f9
chiyu1996 <172302374@qq.com>
2021-01-20 00:09:18 +0800

    2020.1.20 628(暴力)

--- editor/cn/[628]三个数的最大乘积.js
+++ editor/cn/[628]三个数的最大乘积.js
@@ -29,22 +29,18 @@
  * @return {number}
  */
 var maximumProduct = function(nums) {
-  let max1 = -Infinity; let max2 = -Infinity; let max3 = -Infinity;
-  let min1 = Infinity; let min2 = Infinity;
+  let [max1, max2, max3] = [-Infinity, -Infinity, -Infinity];
+  let [min1, min2] = [Infinity, Infinity];
   for (const num of nums) {
     if (num < min1) {
-      min2 = min1;
-      min1 = num;
+      [min2, min1] = [min1, num];
     } else if (num < min2) {
       min2 = num;
     }
     if (num > max1) {
-      max3 = max2;
-      max2 = max1
-      max1 = num;
+      [max3, max2, max1] = [max2, max1, num];
     } else if (num > max2) {
-      max3 = max2;
-      max2 = num;
+      [max3, max2] = [max2, num];
     } else if (num > max3) {
       max3 = num;
     }
